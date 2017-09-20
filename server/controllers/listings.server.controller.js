242b1bb42a8fd7da158212682bb65c452ec3a4c3
javi19 <javibear19@ufl.edu>
2017-09-19 22:39:14 -0400

    Final Product

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -50,6 +50,26 @@ exports.update = function(req, res) {
   /* Replace the article's properties with the new properties found in req.body */
   /* save the coordinates (located in req.results if there is an address property) */
   /* Save the article */
+  listing.code = req.body.code;
+  listing.name = req.body.name;
+  listing.address = req.body.address;
+
+  if(req.results){
+    listing.coordinates = {
+      latitude: req.results.lat,
+       longitude: req.results.lng
+    };
+  }
+
+  listing.save(function(err){
+    if(err){
+      console.log(err);
+      res.status(400).send(err);
+    }
+    else {
+      res.json(listing);
+    }
+  });
 };
 
 /* Delete a listing */
@@ -57,17 +77,33 @@ exports.delete = function(req, res) {
   var listing = req.listing;
 
   /* Remove the article */
+  listing.remove(function(err){
+      if(err){
+          res.status(400).send(err);
+      }
+      else {
+          res.end();
+      }
+  })
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
   /* Your code here */
+  Listing.find().sort('code').exec(function(err, listings){
+      if(err){
+          res.status(400).send(err);
+      }
+      else {
+          res.json(listings);
+      }
+  });
 };
 
 /* 
   Middleware: find a listing by its ID, then pass it to the next request handler. 
 
-  HINT: Find the listing using a mongoose query, 
+  HINT: Find the listing using a mongoose query,
         bind it to the request object as the property 'listing', 
         then finally call next
  */
